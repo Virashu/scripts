b0b719a26acc8f87c74c5087e3d2a993b411ad1e
Virashu <svladichek@gmail.com>
2023-06-03 02:24:51 +0300

    Updated & fixed screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -6,19 +6,17 @@ dest="$dir/%Y-%m-%d-%s_\$wx\$h.jpg"
 
 function get_last {
 	find /home/virashu/Pictures/screenshots -maxdepth 1 -type f -printf '%T@ %p\n' | sort -k1,1nr | head -1 | awk '{print $2}'
-	#find $dir -maxdepth 1 -type f | sort -k1,1nr | head -1
-	#stat -c '%Y %n' $dir/* | sort -k1,1nr | head -1
 }
 
 case $1 in
 	normal)
-		/usr/bin/scrot -d0 "$dest"
+		/usr/bin/scrot -d0 "$dir/$dest"
 		;;
 	focused)
-		/usr/bin/scrot -d0 --focused "$dest"
+		/usr/bin/scrot -d0 --focused "$dir/$dest"
 		;;
 	selection)
-		/usr/bin/scrot -d0 --select "$dest"
+		/usr/bin/scrot -d0 --select "$dir/$dest"
 		;;
 esac
 
